40f55b0fb94564949124877f6d4da83d3e9e9213
Lukas Planz <lukas.planz@web.de>
2019-09-28 13:23:34 +0200

    Fixed ItemStacks class
    Made class and methods package private and implemented limited lore line length

--- src/main/java/de/md5lukas/waypoints/gui/ItemStacks.java
+++ src/main/java/de/md5lukas/waypoints/gui/ItemStacks.java
@@ -8,171 +8,171 @@ import static de.md5lukas.waypoints.Messages.*;
 import static de.md5lukas.waypoints.Waypoints.message;
 import static de.md5lukas.waypoints.store.WPConfig.inventory;
 
-public class ItemStacks {
+class ItemStacks {
 
-	public static ItemStack getPreviousItem(Player p) {
+	static ItemStack getPreviousItem(Player p) {
 		return new ItemBuilder(inventory().getGeneralPreviousItem()).name(message(INVENTORY_GENERAL_PREVIOUS_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_GENERAL_PREVIOUS_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_GENERAL_PREVIOUS_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getNextItem(Player p) {
+	static ItemStack getNextItem(Player p) {
 		return new ItemBuilder(inventory().getGeneralNextItem()).name(message(INVENTORY_GENERAL_NEXT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_GENERAL_NEXT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_GENERAL_NEXT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getBackItem(Player p) {
+	static ItemStack getBackItem(Player p) {
 		return new ItemBuilder(inventory().getGeneralBackItem()).name(message(INVENTORY_GENERAL_BACK_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_GENERAL_BACK_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_GENERAL_BACK_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getOverviewBackgroundItem(Player p) {
+	static ItemStack getOverviewBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getOverviewBackgroundItem()).name(message(INVENTORY_OVERVIEW_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_OVERVIEW_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_OVERVIEW_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getOverviewDeselectItem(Player p) {
+	static ItemStack getOverviewDeselectItem(Player p) {
 		return new ItemBuilder(inventory().getOverviewDeselectItem()).name(message(INVENTORY_OVERVIEW_DESELECT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_OVERVIEW_DESELECT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_OVERVIEW_DESELECT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getOverviewToggleGlobalsShownItem(Player p) {
+	static ItemStack getOverviewToggleGlobalsShownItem(Player p) {
 		return new ItemBuilder(inventory().getOverviewToggleGlobalsItem()).name(message(INVENTORY_OVERVIEW_TOGGLE_GLOBALS_SHOWN_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_OVERVIEW_TOGGLE_GLOBALS_SHOWN_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_OVERVIEW_TOGGLE_GLOBALS_SHOWN_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getOverviewToggleGlobalsHiddenItem(Player p) {
+	static ItemStack getOverviewToggleGlobalsHiddenItem(Player p) {
 		return new ItemBuilder(inventory().getOverviewToggleGlobalsItem()).name(message(INVENTORY_OVERVIEW_TOGGLE_GLOBALS_HIDDEN_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_OVERVIEW_TOGGLE_GLOBALS_HIDDEN_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_OVERVIEW_TOGGLE_GLOBALS_HIDDEN_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointDeathBackgroundItem(Player p) {
+	static ItemStack getWaypointDeathBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointDeathBackgroundItem()).name(message(INVENTORY_WAYPOINT_DEATH_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_DEATH_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_DEATH_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointDeathSelectItem(Player p) {
+	static ItemStack getWaypointDeathSelectItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointDeathSelectItem()).name(message(INVENTORY_WAYPOINT_DEATH_SELECT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_DEATH_SELECT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_DEATH_SELECT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
 	public static ItemStack getWaypointDeathTeleportItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointDeathTeleportItem()).name(message(INVENTORY_WAYPOINT_DEATH_TELEPORT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_DEATH_TELEPORT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_DEATH_TELEPORT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPrivateBackgroundItem(Player p) {
+	static ItemStack getWaypointPrivateBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPrivateBackgroundItem()).name(message(INVENTORY_WAYPOINT_PRIVATE_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PRIVATE_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PRIVATE_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPrivateSelectItem(Player p) {
+	static ItemStack getWaypointPrivateSelectItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPrivateSelectItem()).name(message(INVENTORY_WAYPOINT_PRIVATE_SELECT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PRIVATE_SELECT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PRIVATE_SELECT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPrivateDeleteItem(Player p) {
+	static ItemStack getWaypointPrivateDeleteItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPrivateDeleteItem()).name(message(INVENTORY_WAYPOINT_PRIVATE_DELETE_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PRIVATE_DELETE_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PRIVATE_DELETE_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPrivateRenameItem(Player p) {
+	static ItemStack getWaypointPrivateRenameItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPrivateRenameItem()).name(message(INVENTORY_WAYPOINT_PRIVATE_RENAME_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PRIVATE_RENAME_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PRIVATE_RENAME_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPrivateMoveToFolderItem(Player p) {
+	static ItemStack getWaypointPrivateMoveToFolderItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPrivateMoveToFolderItem()).name(message(INVENTORY_WAYPOINT_PRIVATE_MOVE_TO_FOLDER_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PRIVATE_MOVE_TO_FOLDER_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PRIVATE_MOVE_TO_FOLDER_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPrivateTeleportItem(Player p) {
+	static ItemStack getWaypointPrivateTeleportItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPrivateTeleportItem()).name(message(INVENTORY_WAYPOINT_PRIVATE_TELEPORT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PRIVATE_TELEPORT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PRIVATE_TELEPORT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
 
-	public static ItemStack getWaypointPublicBackgroundItem(Player p) {
+	static ItemStack getWaypointPublicBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPublicBackgroundItem()).name(message(INVENTORY_WAYPOINT_PUBLIC_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PUBLIC_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PUBLIC_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPublicSelectItem(Player p) {
+	static ItemStack getWaypointPublicSelectItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPublicSelectItem()).name(message(INVENTORY_WAYPOINT_PUBLIC_SELECT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PUBLIC_SELECT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PUBLIC_SELECT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPublicDeleteItem(Player p) {
+	static ItemStack getWaypointPublicDeleteItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPublicDeleteItem()).name(message(INVENTORY_WAYPOINT_PUBLIC_DELETE_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PUBLIC_DELETE_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PUBLIC_DELETE_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPublicRenameItem(Player p) {
+	static ItemStack getWaypointPublicRenameItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPublicRenameItem()).name(message(INVENTORY_WAYPOINT_PUBLIC_RENAME_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PUBLIC_RENAME_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PUBLIC_RENAME_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPublicTeleportItem(Player p) {
+	static ItemStack getWaypointPublicTeleportItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPublicTeleportItem()).name(message(INVENTORY_WAYPOINT_PUBLIC_TELEPORT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PUBLIC_TELEPORT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PUBLIC_TELEPORT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPermissionBackgroundItem(Player p) {
+	static ItemStack getWaypointPermissionBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPermissionBackgroundItem()).name(message(INVENTORY_WAYPOINT_PERMISSION_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PERMISSION_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PERMISSION_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPermissionSelectItem(Player p) {
+	static ItemStack getWaypointPermissionSelectItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPermissionSelectItem()).name(message(INVENTORY_WAYPOINT_PERMISSION_SELECT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PERMISSION_SELECT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PERMISSION_SELECT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPermissionDeleteItem(Player p) {
+	static ItemStack getWaypointPermissionDeleteItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPermissionDeleteItem()).name(message(INVENTORY_WAYPOINT_PERMISSION_DELETE_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PERMISSION_DELETE_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PERMISSION_DELETE_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPermissionRenameItem(Player p) {
+	static ItemStack getWaypointPermissionRenameItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPermissionRenameItem()).name(message(INVENTORY_WAYPOINT_PERMISSION_RENAME_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PERMISSION_RENAME_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PERMISSION_RENAME_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getWaypointPermissionTeleportItem(Player p) {
+	static ItemStack getWaypointPermissionTeleportItem(Player p) {
 		return new ItemBuilder(inventory().getWaypointPermissionTeleportItem()).name(message(INVENTORY_WAYPOINT_PERMISSION_TELEPORT_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_WAYPOINT_PERMISSION_TELEPORT_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_WAYPOINT_PERMISSION_TELEPORT_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getSelectFolderBackgroundItem(Player p) {
+	static ItemStack getSelectFolderBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getSelectFolderBackgroundItem()).name(message(INVENTORY_SELECT_FOLDER_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_SELECT_FOLDER_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_SELECT_FOLDER_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getSelectFolderNoFolderItem(Player p) {
+	static ItemStack getSelectFolderNoFolderItem(Player p) {
 		return new ItemBuilder(inventory().getSelectFolderNoFolderItem()).name(message(INVENTORY_SELECT_FOLDER_NO_FOLDER_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_SELECT_FOLDER_NO_FOLDER_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_SELECT_FOLDER_NO_FOLDER_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getFolderPrivateBackgroundItem(Player p) {
+	static ItemStack getFolderPrivateBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getFolderPrivateBackgroundItem()).name(message(INVENTORY_FOLDER_PRIVATE_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_FOLDER_PRIVATE_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_FOLDER_PRIVATE_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getFolderPrivateDeleteItem(Player p) {
+	static ItemStack getFolderPrivateDeleteItem(Player p) {
 		return new ItemBuilder(inventory().getFolderPrivateDeleteItem()).name(message(INVENTORY_FOLDER_PRIVATE_DELETE_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_FOLDER_PRIVATE_DELETE_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_FOLDER_PRIVATE_DELETE_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getFolderPrivateRenameItem(Player p) {
+	static ItemStack getFolderPrivateRenameItem(Player p) {
 		return new ItemBuilder(inventory().getFolderPrivateRenameItem()).name(message(INVENTORY_FOLDER_PRIVATE_RENAME_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_FOLDER_PRIVATE_RENAME_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_FOLDER_PRIVATE_RENAME_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getFolderPublicBackgroundItem(Player p) {
+	static ItemStack getFolderPublicBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getFolderPrivateBackgroundItem()).name(message(INVENTORY_FOLDER_PUBLIC_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_FOLDER_PUBLIC_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_FOLDER_PUBLIC_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 
-	public static ItemStack getFolderPermissionBackgroundItem(Player p) {
+	static ItemStack getFolderPermissionBackgroundItem(Player p) {
 		return new ItemBuilder(inventory().getFolderPrivateBackgroundItem()).name(message(INVENTORY_FOLDER_PERMISSION_BACKGROUND_DISPLAY_NAME, p))
-			.lore(message(INVENTORY_FOLDER_PERMISSION_BACKGROUND_DESCRIPTION, p)).make();
+			.lore(message(INVENTORY_FOLDER_PERMISSION_BACKGROUND_DESCRIPTION, p), inventory().getMaxDescriptionLineLength()).make();
 	}
 }
