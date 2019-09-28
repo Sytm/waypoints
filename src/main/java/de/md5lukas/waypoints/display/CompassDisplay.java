b2adff14f1d7c3bf0238cfb61f15ed72b8782998
Lukas Planz <lukas.planz@web.de>
2019-09-28 13:54:57 +0200

    Fixed compass display to handle the different config settings

--- src/main/java/de/md5lukas/waypoints/display/CompassDisplay.java
+++ src/main/java/de/md5lukas/waypoints/display/CompassDisplay.java
@@ -1,10 +1,15 @@
 package de.md5lukas.waypoints.display;
 
 import de.md5lukas.nbt.tags.CompoundTag;
+import de.md5lukas.waypoints.Waypoints;
 import de.md5lukas.waypoints.data.waypoint.Waypoint;
 import de.md5lukas.waypoints.store.LocationTag;
+import de.md5lukas.waypoints.store.WPConfig;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+
+import static de.md5lukas.waypoints.store.WPConfig.displays;
 
 public final class CompassDisplay extends WaypointDisplay {
 
@@ -14,8 +19,10 @@ public final class CompassDisplay extends WaypointDisplay {
 
 	@Override
 	public void show(Player player, Waypoint waypoint) {
-		CompoundTag store = getStore(player);
-		store.put("location", new LocationTag(null, player.getCompassTarget()));
+		if (displays().getCompassDefaultLocationType() == WPConfig.DefaultCompassLocationType.PREVIOUS) {
+			CompoundTag store = getStore(player);
+			store.put("location", new LocationTag(null, player.getCompassTarget()));
+		}
 		player.setCompassTarget(waypoint.getLocation());
 	}
 
@@ -24,10 +31,24 @@ public final class CompassDisplay extends WaypointDisplay {
 
 	@Override
 	public void disable(Player player) {
-		CompoundTag store = getStore(player);
-		if (store.contains("location")) {
-			player.setCompassTarget(((LocationTag) store.get("location")).value());
-			store.put("location", null);
+		switch (displays().getCompassDefaultLocationType()) {
+			case SPAWN:
+				player.setCompassTarget(Bukkit.getWorlds().get(0).getSpawnLocation());
+				break;
+			case CONFIG:
+				player.setCompassTarget(displays().getCompassDefaultLocation());
+				break;
+			case PREVIOUS:
+				CompoundTag store = getStore(player);
+				if (store.contains("location")) {
+					player.setCompassTarget(((LocationTag) store.get("location")).value());
+					store.put("location", null);
+				}
+				break;
+			case INGAME:
+			case INGAME_LOCK:
+				player.setCompassTarget(Waypoints.getGlobalStore().getCompassTarget());
+				break;
 		}
 	}
 }
