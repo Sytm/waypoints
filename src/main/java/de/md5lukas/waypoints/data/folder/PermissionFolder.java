62ff4be3373f0ca849c682c9e0a0b5a1a032c7b4
Lukas Planz <lukas.planz@web.de>
2019-09-29 17:45:31 +0200

    Replaced String#split with StringHelper#split

--- src/main/java/de/md5lukas/waypoints/data/folder/PermissionFolder.java
+++ src/main/java/de/md5lukas/waypoints/data/folder/PermissionFolder.java
@@ -18,6 +18,7 @@
 
 package de.md5lukas.waypoints.data.folder;
 
+import de.md5lukas.commons.StringHelper;
 import de.md5lukas.commons.collections.ReplaceableList;
 import de.md5lukas.nbt.tags.CompoundTag;
 import de.md5lukas.nbt.tags.ListTag;
@@ -73,7 +74,7 @@ public class PermissionFolder extends Folder {
 
 	@Override
 	public List<String> getDescription(Player player) {
-		return ReplaceableList.ofStrings(message(INVENTORY_FOLDER_PERMISSION_DESCRIPTION, player).split("\\n")).replace(
+		return new ReplaceableList(StringHelper.split(message(INVENTORY_FOLDER_PERMISSION_DESCRIPTION, player), '\n')).replace(
 			"%amount%", Integer.toString(waypoints.stream().filter(wp -> player.hasPermission(((PermissionWaypoint) wp).getPermission())).mapToInt(wp -> 1).sum()));
 	}
 
