7920aa4cb832e3e17f16e204f51a055fb8bf7c39
Md5Lukas <git@md5lukas.de>
2019-10-05 19:19:35 +0200

    Update WaypointsCommand class to use the new send syntax

--- src/main/java/de/md5lukas/waypoints/command/WaypointsCommand.java
+++ src/main/java/de/md5lukas/waypoints/command/WaypointsCommand.java
@@ -60,7 +60,7 @@ public class WaypointsCommand implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			if (!p.hasPermission("waypoints.command")) {
-				p.sendMessage(GENERAL_NO_PERMISSION.getRaw(p));
+				GENERAL_NO_PERMISSION.send(p);
 				return true;
 			}
 			if (args.length == 0) {
@@ -70,110 +70,110 @@ public class WaypointsCommand implements CommandExecutor {
 			switch (args[0].toLowerCase()) {
 				case "help": {
 					if (!p.hasPermission("waypoints.help")) {
-						p.sendMessage(GENERAL_NO_PERMISSION.getRaw(p));
+						GENERAL_NO_PERMISSION.send(p);
 						return true;
 					}
-					p.sendMessage(COMMAND_HELP_TITLE.getRaw(p));
-					p.sendMessage(COMMAND_HELP_HELP.getRaw(p));
+					COMMAND_HELP_TITLE.send(p);
+					COMMAND_HELP_HELP.send(p);
 					if (p.hasPermission("waypoints.set.private"))
-						p.sendMessage(COMMAND_HELP_SET_PRIVATE.getRaw(p));
+						COMMAND_HELP_SET_PRIVATE.send(p);
 					if (p.hasPermission("waypoints.set.public"))
-						p.sendMessage(COMMAND_HELP_SET_PUBLIC.getRaw(p));
+						COMMAND_HELP_SET_PUBLIC.send(p);
 					if (p.hasPermission("waypoints.set.public"))
-						p.sendMessage(COMMAND_HELP_SET_PERMISSION.getRaw(p));
+						COMMAND_HELP_SET_PERMISSION.send(p);
 					if (p.hasPermission("waypoints.compass"))
-						p.sendMessage(COMMAND_HELP_COMPASS.getRaw(p));
+						COMMAND_HELP_COMPASS.send(p);
 					if (p.hasPermission("waypoints.other"))
-						p.sendMessage(COMMAND_HELP_OTHER.getRaw(p));
+						COMMAND_HELP_OTHER.send(p);
 					if (WPConfig.inventory().isCustomItemEnabled())
-						p.sendMessage(COMMAND_HELP_UPDATE_ITEM.getRaw(p));
+						COMMAND_HELP_UPDATE_ITEM.send(p);
 					if ((WPConfig.allowRenamingWaypointsPrivate() || WPConfig.allowRenamingWaypointsPublic()
 						|| WPConfig.allowRenamingWaypointsPermission()) && WPConfig.allowRenamingFoldersPrivate())
-						p.sendMessage(COMMAND_HELP_RENAME_NORMAL.getRaw(p));
+						COMMAND_HELP_RENAME_NORMAL.send(p);
 					else if (WPConfig.allowRenamingWaypointsPrivate() || WPConfig.allowRenamingWaypointsPublic() || WPConfig.allowRenamingWaypointsPermission())
-						p.sendMessage(COMMAND_HELP_RENAME_WAYPOINT_ONLY.getRaw(p));
+						COMMAND_HELP_RENAME_WAYPOINT_ONLY.send(p);
 					else if (WPConfig.allowRenamingFoldersPrivate())
-						p.sendMessage(COMMAND_HELP_RENAME_FOLDER_ONLY.getRaw(p));
+						COMMAND_HELP_RENAME_FOLDER_ONLY.send(p);
 					break;
 				}
 				case "compass": {
 					if (!p.hasPermission("waypoints.compass")) {
-						p.sendMessage(GENERAL_NO_PERMISSION.getRaw(p));
+						GENERAL_NO_PERMISSION.send(p);
 						return true;
 					}
 					switch (WPConfig.displays().getCompassDefaultLocationType()) {
 						case INGAME:
 							Waypoints.getGlobalStore().setCompassTarget(p.getLocation());
 							Waypoints.getGlobalStore().save(true);
-							p.sendMessage(COMMAND_COMPASS_SET_SUCCESS.getRaw(p));
+							COMMAND_COMPASS_SET_SUCCESS.send(p);
 							break;
 						case INGAME_LOCK:
-							p.sendMessage(COMMAND_COMPASS_LOCKED.getRaw(p));
+							COMMAND_COMPASS_LOCKED.send(p);
 							break;
 						default:
-							p.sendMessage(COMMAND_COMPASS_DISABLED.getRaw(p));
+							COMMAND_COMPASS_DISABLED.send(p);
 							break;
 					}
 					break;
 				}
 				case "set": {
 					if (!p.hasPermission("waypoints.set.private")) {
-						p.sendMessage(GENERAL_NO_PERMISSION.getRaw(p));
+						GENERAL_NO_PERMISSION.send(p);
 						return true;
 					}
 					if (args.length <= 1) {
-						p.sendMessage(COMMAND_SET_PRIVATE_WRONG_USAGE.getRaw(p));
+						COMMAND_SET_PRIVATE_WRONG_USAGE.send(p);
 						return true;
 					}
 					WPPlayerData data = WPPlayerData.getPlayerData(p.getUniqueId());
 					String name = StringHelper.buildStringFromArray(args, 1);
 					if (!WPConfig.allowDuplicateWaypointNamesPrivate()) {
 						if (data.findWaypoint(wp -> wp.getName().equalsIgnoreCase(name)).isPresent()) {
-							p.sendMessage(COMMAND_SET_PRIVATE_NAME_DUPLICATE.getRaw(p));
+							COMMAND_SET_PRIVATE_NAME_DUPLICATE.send(p);
 							return true;
 						}
 					}
 					if (WPConfig.getWaypointLimit() > 0) {
 						if (data.countWaypoints() >= WPConfig.getWaypointLimit()) {
-							p.sendMessage(COMMAND_SET_PRIVATE_LIMIT_REACHED.getRaw(p));
+							COMMAND_SET_PRIVATE_LIMIT_REACHED.send(p);
 							return true;
 						}
 					}
 					data.addWaypoint(new PrivateWaypoint(name, p.getLocation()));
-					p.sendMessage(COMMAND_SET_PRIVATE_SUCCESS.getRaw(p));
+					COMMAND_SET_PRIVATE_SUCCESS.send(p);
 					break;
 				}
 				case "setpub":
 				case "setpublic": {
 					if (!p.hasPermission("waypoints.set.public")) {
-						p.sendMessage(GENERAL_NO_PERMISSION.getRaw(p));
+						GENERAL_NO_PERMISSION.send(p);
 						return true;
 					}
 					if (args.length <= 1) {
-						p.sendMessage(COMMAND_SET_PUBLIC_WRONG_USAGE.getRaw(p));
+						COMMAND_SET_PUBLIC_WRONG_USAGE.send(p);
 						return true;
 					}
 					String name = StringHelper.buildStringFromArray(args, 1);
 					if (!WPConfig.allowDuplicateWaypointNamesPublic()) {
 						for (Waypoint wp : Waypoints.getGlobalStore().getPublicFolder().getWaypoints(p)) {
 							if (wp.getName().equalsIgnoreCase(name)) {
-								p.sendMessage(COMMAND_SET_PUBLIC_NAME_DUPLICATE.getRaw(p));
+								COMMAND_SET_PUBLIC_NAME_DUPLICATE.send(p);
 								return true;
 							}
 						}
 					}
 					Waypoints.getGlobalStore().getPublicFolder().addWaypoint(new PublicWaypoint(name, p.getLocation()));
-					p.sendMessage(COMMAND_SET_PUBLIC_SUCCESS.getRaw(p));
+					COMMAND_SET_PUBLIC_SUCCESS.send(p);
 					break;
 				}
 				case "setperm":
 				case "setpermission": {
 					if (!p.hasPermission("waypoints.set.permission")) {
-						p.sendMessage(GENERAL_NO_PERMISSION.getRaw(p));
+						GENERAL_NO_PERMISSION.send(p);
 						return true;
 					}
 					if (args.length <= 2) {
-						p.sendMessage(COMMAND_SET_PERMISSION_WRONG_USAGE.getRaw(p));
+						COMMAND_SET_PERMISSION_WRONG_USAGE.send(p);
 						return true;
 					}
 					String permission = args[1];
@@ -181,42 +181,42 @@ public class WaypointsCommand implements CommandExecutor {
 					if (!WPConfig.allowDuplicateWaypointNamesPermission()) {
 						for (Waypoint wp : Waypoints.getGlobalStore().getPermissionFolder().getWaypoints(p)) {
 							if (wp.getName().equalsIgnoreCase(name)) {
-								p.sendMessage(COMMAND_SET_PERMISSION_NAME_DUPLICATE.getRaw(p));
+								COMMAND_SET_PERMISSION_NAME_DUPLICATE.send(p);
 								return true;
 							}
 						}
 					}
 					Waypoints.getGlobalStore().getPermissionFolder().addWaypoint(new PermissionWaypoint(name, p.getLocation(), permission));
-					p.sendMessage(COMMAND_SET_PERMISSION_SUCCESS.getRaw(p));
+					COMMAND_SET_PERMISSION_SUCCESS.send(p);
 					break;
 				}
 				case "cf":
 				case "createfolder": {
 					if (args.length <= 1) {
-						p.sendMessage(COMMAND_CREATE_FOLDER_WRONG_USAGE.getRaw(p));
+						COMMAND_CREATE_FOLDER_WRONG_USAGE.send(p);
 						return true;
 					}
 					WPPlayerData data = WPPlayerData.getPlayerData(p.getUniqueId());
 					String name = StringHelper.buildStringFromArray(args, 1);
 					if (!WPConfig.allowDuplicateFolderPrivateNames()) {
 						if (data.findFolder(f -> f.getName().equalsIgnoreCase(name)).isPresent()) {
-							p.sendMessage(COMMAND_CREATE_FOLDER_NAME_DUPLICATE.getRaw(p));
+							COMMAND_CREATE_FOLDER_NAME_DUPLICATE.send(p);
 							return true;
 						}
 					}
 					data.addFolder(name);
-					p.sendMessage(COMMAND_CREATE_FOLDER_SUCCESS.getRaw(p));
+					COMMAND_CREATE_FOLDER_SUCCESS.send(p);
 					break;
 				}
 				case "other": {
 					if (args.length <= 1) {
-						p.sendMessage(COMMAND_OTHER_WRONG_USAGE.getRaw(p));
+						COMMAND_OTHER_WRONG_USAGE.send(p);
 						return true;
 					}
 					if (StringHelper.MC_USERNAME_PATTERN.matcher(args[1]).matches()) {
 						UUIDUtils.getUUID(args[1], uuid -> {
 							if (uuid == null) {
-								p.sendMessage(COMMAND_OTHER_PLAYER_NAME_NOT_FOUND.getRaw(p));
+								COMMAND_OTHER_PLAYER_NAME_NOT_FOUND.send(p);
 								return;
 							}
 							GUIManager.openGUI(p, uuid);
@@ -225,34 +225,34 @@ public class WaypointsCommand implements CommandExecutor {
 						UUID uuid = UUID.fromString(args[1]);
 						UUIDUtils.getName(uuid, name -> {
 							if (name == null) {
-								p.sendMessage(COMMAND_OTHER_UUID_NOT_FOUND.getRaw(p));
+								COMMAND_OTHER_UUID_NOT_FOUND.send(p);
 								return;
 							}
 							GUIManager.openGUI(p, uuid);
 						});
 					} else {
-						p.sendMessage(COMMAND_OTHER_NOT_A_VALID_UUID_OR_PLAYER.getRaw(p));
+						COMMAND_OTHER_NOT_A_VALID_UUID_OR_PLAYER.send(p);
 						return true;
 					}
 					break;
 				}
 				case "updateitem": {
 					if (!WPConfig.inventory().isCustomItemEnabled()) {
-						p.sendMessage(COMMAND_UPDATE_ITEM_DISABLED.getRaw(p));
+						COMMAND_UPDATE_ITEM_DISABLED.send(p);
 						return true;
 					}
 					if (args.length <= 2) {
-						p.sendMessage(COMMAND_UPDATE_ITEM_WRONG_USAGE.getRaw(p));
+						COMMAND_UPDATE_ITEM_WRONG_USAGE.send(p);
 						return true;
 					}
 					if (!UUIDUtils.isUUID(args[2])) {
-						p.sendMessage(GENERAL_NOT_A_VALID_UUID.getRaw(p));
+						GENERAL_NOT_A_VALID_UUID.send(p);
 						return true;
 					}
 					Material mat = p.getInventory().getItemInMainHand().getType();
 					UUID uuid = UUID.fromString(args[2]);
 					if (!WPConfig.inventory().isValidCustomItem(mat)) {
-						p.sendMessage(COMMAND_UPDATE_ITEM_NOT_A_VALID_ITEM.getRaw(p));
+						COMMAND_UPDATE_ITEM_NOT_A_VALID_ITEM.send(p);
 						return true;
 					}
 					if ("waypoint".equalsIgnoreCase(args[1])) {
@@ -260,36 +260,36 @@ public class WaypointsCommand implements CommandExecutor {
 						Optional<Waypoint> waypoint = playerData.findWaypoint(wp -> wp.getID().equals(uuid));
 						if (waypoint.isPresent()) {
 							waypoint.get().setMaterial(mat);
-							p.sendMessage(COMMAND_UPDATE_ITEM_WAYPOINT_SUCCESS.getRaw(p));
+							COMMAND_UPDATE_ITEM_WAYPOINT_SUCCESS.send(p);
 						} else {
-							p.sendMessage(GENERAL_WAYPOINT_NOT_FOUND.getRaw(p));
+							GENERAL_WAYPOINT_NOT_FOUND.send(p);
 						}
 					} else if ("folder".equalsIgnoreCase(args[1])) {
 						WPPlayerData playerData = WPPlayerData.getPlayerData(p.getUniqueId());
 						Optional<Folder> folder = playerData.findFolder(f -> f.getID().equals(uuid));
 						if (folder.isPresent()) {
 							folder.get().setMaterial(mat);
-							p.sendMessage(COMMAND_UPDATE_ITEM_FOLDER_SUCCESS.getRaw(p));
+							COMMAND_UPDATE_ITEM_FOLDER_SUCCESS.send(p);
 						} else {
-							p.sendMessage(GENERAL_FOLDER_NOT_FOUND.getRaw(p));
+							GENERAL_FOLDER_NOT_FOUND.send(p);
 						}
 					} else {
-						p.sendMessage(COMMAND_UPDATE_ITEM_WRONG_USAGE.getRaw(p));
+						COMMAND_UPDATE_ITEM_WRONG_USAGE.send(p);
 					}
 					break;
 				}
 				case "rename": {
 					if (!WPConfig.allowRenamingWaypointsPrivate() && !WPConfig.allowRenamingWaypointsPublic()
 						&& !WPConfig.allowRenamingWaypointsPermission() && !WPConfig.allowRenamingFoldersPrivate()) {
-						p.sendMessage(COMMAND_RENAME_DISABLED.getRaw(p));
+						COMMAND_RENAME_DISABLED.send(p);
 						return true;
 					}
 					if (args.length <= 3) {
-						p.sendMessage(COMMAND_RENAME_WRONG_USAGE.getRaw(p));
+						COMMAND_RENAME_WRONG_USAGE.send(p);
 						return true;
 					}
 					if (!UUIDUtils.isUUID(args[2])) {
-						p.sendMessage(GENERAL_NOT_A_VALID_UUID.getRaw(p));
+						GENERAL_NOT_A_VALID_UUID.send(p);
 						return true;
 					}
 					UUID uuid = UUID.fromString(args[2]);
@@ -297,94 +297,102 @@ public class WaypointsCommand implements CommandExecutor {
 					switch (args[1].toLowerCase()) {
 						case "privatewaypoint": {
 							if (!WPConfig.allowRenamingWaypointsPrivate()) {
-								p.sendMessage(COMMAND_RENAME_WAYPOINT_PRIVATE_DISABLED.getRaw(p));
+								COMMAND_RENAME_WAYPOINT_PRIVATE_DISABLED.send(p);
 								return true;
 							}
 							WPPlayerData data = WPPlayerData.getPlayerData(p.getUniqueId());
 							if (!WPConfig.allowDuplicateWaypointNamesPrivate()) {
 								if (data.findWaypoint(wp -> newName.equalsIgnoreCase(wp.getName())).isPresent()) {
-									p.sendMessage(COMMAND_RENAME_WAYPOINT_PRIVATE_NAME_DUPLICATE.getRaw(p));
+									COMMAND_RENAME_WAYPOINT_PRIVATE_NAME_DUPLICATE.send(p);
 									return true;
 								}
 							}
 							Optional<Waypoint> waypoint = data.findWaypoint(wp -> uuid.equals(wp.getID()));
 							if (!waypoint.isPresent()) {
-								p.sendMessage(GENERAL_WAYPOINT_NOT_FOUND.getRaw(p));
+								GENERAL_WAYPOINT_NOT_FOUND.send(p);
 								return true;
 							}
 							waypoint.get().setName(newName);
-							p.sendMessage(COMMAND_RENAME_WAYPOINT_SUCCESS.getRaw(p));
+							COMMAND_RENAME_WAYPOINT_SUCCESS.send(p);
 							break;
 						}
 						case "publicwaypoint": {
+							if (!p.hasPermission("waypoints.rename.public")) {
+								GENERAL_NO_PERMISSION.send(p);
+								return true;
+							}
 							if (!WPConfig.allowRenamingWaypointsPublic()) {
-								p.sendMessage(COMMAND_RENAME_WAYPOINT_PUBLIC_DISABLED.getRaw(p));
+								COMMAND_RENAME_WAYPOINT_PUBLIC_DISABLED.send(p);
 								return true;
 							}
 							if (!WPConfig.allowDuplicateWaypointNamesPublic()) {
 								if (Waypoints.getGlobalStore().getPublicFolder().findWaypoint(wp -> newName.equalsIgnoreCase(wp.getName())).isPresent()) {
-									p.sendMessage(COMMAND_RENAME_WAYPOINT_PUBLIC_NAME_DUPLICATE.getRaw(p));
+									COMMAND_RENAME_WAYPOINT_PUBLIC_NAME_DUPLICATE.send(p);
 									return true;
 								}
 							}
 							Optional<Waypoint> waypoint = Waypoints.getGlobalStore().getPublicFolder().findWaypoint(wp -> uuid.equals(wp.getID()));
 							if (!waypoint.isPresent()) {
-								p.sendMessage(GENERAL_WAYPOINT_NOT_FOUND.getRaw(p));
+								GENERAL_WAYPOINT_NOT_FOUND.send(p);
 								return true;
 							}
 							waypoint.get().setName(newName);
-							p.sendMessage(COMMAND_RENAME_WAYPOINT_SUCCESS.getRaw(p));
+							COMMAND_RENAME_WAYPOINT_SUCCESS.send(p);
 							break;
 						}
 						case "permissionwaypoint": {
+							if (!p.hasPermission("waypoints.rename.permission")) {
+								GENERAL_NO_PERMISSION.send(p);
+								return true;
+							}
 							if (!WPConfig.allowRenamingWaypointsPermission()) {
-								p.sendMessage(COMMAND_RENAME_WAYPOINT_PERMISSION_DISABLED.getRaw(p));
+								COMMAND_RENAME_WAYPOINT_PERMISSION_DISABLED.send(p);
 								return true;
 							}
 							if (!WPConfig.allowDuplicateWaypointNamesPermission()) {
 								if (Waypoints.getGlobalStore().getPermissionFolder().findWaypoint(wp -> newName.equalsIgnoreCase(wp.getName())).isPresent()) {
-									p.sendMessage(COMMAND_RENAME_WAYPOINT_PERMISSION_NAME_DUPLICATE.getRaw(p));
+									COMMAND_RENAME_WAYPOINT_PERMISSION_NAME_DUPLICATE.send(p);
 									return true;
 								}
 							}
 							Optional<Waypoint> waypoint = Waypoints.getGlobalStore().getPermissionFolder().findWaypoint(wp -> uuid.equals(wp.getID()));
 							if (!waypoint.isPresent()) {
-								p.sendMessage(GENERAL_WAYPOINT_NOT_FOUND.getRaw(p));
+								GENERAL_WAYPOINT_NOT_FOUND.send(p);
 								return true;
 							}
 							waypoint.get().setName(newName);
-							p.sendMessage(COMMAND_RENAME_WAYPOINT_SUCCESS.getRaw(p));
+							COMMAND_RENAME_WAYPOINT_SUCCESS.send(p);
 							break;
 						}
 						case "folder": {
 							WPPlayerData data = WPPlayerData.getPlayerData(p.getUniqueId());
 							if (!WPConfig.allowDuplicateFolderPrivateNames()) {
 								if (data.findFolder(f -> f.getName().equalsIgnoreCase(newName)).isPresent()) {
-									p.sendMessage(COMMAND_RENAME_FOLDER_NAME_DUPLICATE.getRaw(p));
+									COMMAND_RENAME_FOLDER_NAME_DUPLICATE.send(p);
 									return true;
 								}
 							}
 							Optional<Folder> folder = data.findFolder(f -> f.getID().equals(uuid));
 							if (!folder.isPresent()) {
-								p.sendMessage(GENERAL_FOLDER_NOT_FOUND.getRaw(p));
+								GENERAL_FOLDER_NOT_FOUND.send(p);
 								return true;
 							}
 							folder.get().setName(newName);
-							p.sendMessage(COMMAND_RENAME_FOLDER_SUCCESS.getRaw(p));
+							COMMAND_RENAME_FOLDER_SUCCESS.send(p);
 							break;
 						}
 						default:
-							p.sendMessage(COMMAND_RENAME_WRONG_USAGE.getRaw(p));
+							COMMAND_RENAME_WRONG_USAGE.send(p);
 							break;
 					}
 					break;
 				}
 				default:
-					p.sendMessage(COMMAND_NOT_FOUND.getRaw(p));
+					COMMAND_NOT_FOUND.send(p);
 					break;
 			}
 		} else {
-			sender.sendMessage(GENERAL_NOT_A_PLAYER.getRaw(sender));
+			GENERAL_NOT_A_PLAYER.send(sender);
 		}
 		return true;
 	}
