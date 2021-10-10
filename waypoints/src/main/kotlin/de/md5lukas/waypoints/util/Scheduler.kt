837336775c2ae6a14738989635e386a8b8e3d950
Lukas Planz <lukas.planz@web.de>
2021-10-10 15:32:44 +0200

    Cleanup util package

--- waypoints/src/main/kotlin/de/md5lukas/waypoints/util/Scheduler.kt
+++ waypoints/src/main/kotlin/de/md5lukas/waypoints/util/Scheduler.kt
@@ -2,10 +2,3 @@ package de.md5lukas.waypoints.util
 
 import org.bukkit.plugin.Plugin
 
-fun Plugin.runTask(block: () -> Unit) {
-    server.scheduler.runTask(this, block)
-}
-
-fun Plugin.runTaskAsync(block: () -> Unit) {
-    server.scheduler.runTaskAsynchronously(this, block)
-}
