3cdaf29dc31c7c2fdc5204f0a9a57714f035a875
Md5Lukas <git@md5lukas.de>
2023-03-12 15:12:03 +0100

    Print warning for missing world translation only once

--- waypoints/src/main/kotlin/de/md5lukas/waypoints/lang/WorldTranslations.kt
+++ waypoints/src/main/kotlin/de/md5lukas/waypoints/lang/WorldTranslations.kt
@@ -1,18 +1,36 @@
 package de.md5lukas.waypoints.lang
 
+import de.md5lukas.waypoints.events.ConfigReloadEvent
+import de.md5lukas.waypoints.util.registerEvents
 import org.bukkit.World
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
 import java.util.logging.Level
 
 class WorldTranslations(
     private val tl: TranslationLoader
-) {
+) : Listener {
+
+    init {
+        tl.plugin.registerEvents(this)
+    }
+
+    private val warned: MutableSet<World> = mutableSetOf()
+
     fun getWorldName(world: World): String {
         val key = "worlds.${world.name}"
         return if (key in tl) {
             tl[key]
         } else {
-            tl.plugin.logger.log(Level.WARNING, "The world ${world.name} has no translation present. Using actual name as a fallback.")
+            if (warned.add(world)) {
+                tl.plugin.logger.log(Level.WARNING, "The world ${world.name} has no translation present. Using actual name as a fallback.")
+            }
             world.name
         }
+    }
+
+    @EventHandler
+    private fun onReload(e: ConfigReloadEvent) {
+        warned.clear()
     }
 }
