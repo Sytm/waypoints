1eb713b5a3e2c69588b0655218f671c46eb511f5
Lukas Planz <lukas.planz@web.de>
2023-04-02 14:10:52 +0200

    Make command suggestion tooltips customizable through translations

--- utils/src/main/kotlin/de/md5lukas/waypoints/util/Formatters.kt
+++ utils/src/main/kotlin/de/md5lukas/waypoints/util/Formatters.kt
@@ -3,14 +3,9 @@ package de.md5lukas.waypoints.util
 import de.md5lukas.commons.MathHelper
 import java.text.CharacterIterator
 import java.text.StringCharacterIterator
-import java.time.format.DateTimeFormatter
 import kotlin.math.abs
 
 fun Double.format(): String = MathHelper.format(this)
-
-object Formatters {
-    val SHORT_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.uuuu kk:mm")!!
-}
 
 // Based on https://stackoverflow.com/a/3758880
 fun Long.humanReadableByteCountBin(): String {
