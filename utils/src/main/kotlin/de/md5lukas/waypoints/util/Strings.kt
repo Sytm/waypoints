2091bb6837f0491136e5e283e8f2c05a00f85ae6
Lukas Planz <lukas.planz@web.de>
2023-04-02 14:11:34 +0200

    Remove no longer used methods

--- utils/src/main/kotlin/de/md5lukas/waypoints/util/Strings.kt
+++ utils/src/main/kotlin/de/md5lukas/waypoints/util/Strings.kt
@@ -1,46 +1,12 @@
 package de.md5lukas.waypoints.util
 
-import org.bukkit.ChatColor
 import kotlin.math.max
 import kotlin.math.min
 
 private val minecraftUsernamePattern = Regex("^\\w{3,16}$")
 private val nonWordCharacterRegex = Regex("\\W")
-private val aotVariablePattern = Regex("\\$\\[(\\w+)]")
-private val runtimeVariablePattern = Regex("\\$\\{(\\w+)}")
-
-fun String.translateColorCodes(): String = ChatColor.translateAlternateColorCodes('&', this)
-
-fun String.aotReplace(map: Map<String, String>): String {
-    return replace(aotVariablePattern, map)
-}
-
-fun String.runtimeReplace(map: Map<String, String>): String {
-    return replace(runtimeVariablePattern, map)
-}
 
 fun String.isMinecraftUsername() = this.matches(minecraftUsernamePattern)
-
-private fun String.replace(regex: Regex, map: Map<String, String>): String {
-    return regex.replace(this) { map[it.groupValues[1]] ?: it.groupValues[0] }
-}
-
-fun splitDescriptionStringToList(description: String): List<String> {
-    val rawLines = description.lines()
-
-    val lines = ArrayList<String>()
-    var lastColorCodes = ""
-
-    rawLines.forEachIndexed { index, line ->
-        if (index == rawLines.size - 1 && line.trim().isEmpty()) {
-            return@forEachIndexed
-        }
-        lines.add(lastColorCodes + line)
-        lastColorCodes = ChatColor.getLastColors(line)
-    }
-
-    return lines
-}
 
 fun String.loopingSubstring(size: Int, offset: Int): String {
     val modOffset = Math.floorMod(offset, length)
--- waypoints/src/main/kotlin/de/md5lukas/waypoints/util/KyoriHelper.kt
+++ waypoints/src/main/kotlin/de/md5lukas/waypoints/util/KyoriHelper.kt
@@ -3,12 +3,7 @@ package de.md5lukas.waypoints.util
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.tag.resolver.Formatter
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 import java.time.temporal.TemporalAccessor
-
-fun literalReplacements(vararg replacements: Pair<String, String>): Array<TagResolver> = replacements.map {
-    it.first placeholder it.second
-}.toTypedArray()
 
 infix fun String.placeholder(component: Component) = Placeholder.component(this, component)
 infix fun String.placeholder(text: String) = Placeholder.unparsed(this, text)
