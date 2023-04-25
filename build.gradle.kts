fa91a0f4eb25a9f796d259ba6b1c62b6ebc37503
Lukas Planz <lukas.planz@web.de>
2023-04-25 17:38:35 +0200

    Backport c973f078

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") apply false
     id("com.github.johnrengelman.shadow") apply false
     id("org.jetbrains.dokka") apply false
+}
+
+subprojects {
+    tasks.withType<KotlinCompile> {
+        compilerOptions.freeCompilerArgs.addAll(
+            "-Xjvm-default=all",
+            "-Xlambdas=indy",
+        )
+    }
 }
