42dd8109dae98a54710a97ec61aa110c720c2afa
Lukas Planz <lukas.planz@web.de>
2023-04-26 16:39:02 +0200

    Readd using command label in help

---
+++ waypoints/src/main/kotlin/de/md5lukas/waypoints/util/CommandAPI.kt
@@ -0,0 +1,7 @@
+package de.md5lukas.waypoints.util
+
+import dev.jorel.commandapi.executors.CommandArguments
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
+
+val CommandArguments.labelResolver: TagResolver
+    get() = "label" placeholder fullInput.substringBefore(' ').removePrefix("/")
