4f3427975cbe82e26c709ec9f2b7513a9085228c
Md5Lukas <git@md5lukas.de>
2023-05-21 16:09:15 +0200

    Publish and relocate SignGUI

--- signgui/build.gradle.kts
+++ signgui/build.gradle.kts
@@ -1,7 +1,46 @@
-plugins { `java-library` }
+plugins {
+  `java-library`
+  `maven-publish`
+}
 
 dependencies {
   api(libs.paper)
   compileOnly(libs.annotations)
   implementation(libs.protocollib)
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(libs.versions.jvmToolchain.get().toInt()))
+  withSourcesJar()
+  withJavadocJar()
+}
+
+group = "de.md5lukas"
+
+version = "1.0.0"
+
+publishing {
+  repositories {
+    maven {
+      name = "md5lukasReposilite"
+
+      url =
+        uri(
+          "https://repo.md5lukas.de/${
+            if (version.toString().endsWith("-SNAPSHOT")) {
+              "snapshots"
+            } else {
+              "releases"
+            }
+          }")
+
+      credentials(PasswordCredentials::class)
+      authentication { create<BasicAuthentication>("basic") }
+    }
+  }
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["java"])
+    }
+  }
 }
