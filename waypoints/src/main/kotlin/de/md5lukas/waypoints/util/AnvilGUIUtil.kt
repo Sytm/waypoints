d849b461b87d54155b6c97047994107bd637138a
Md5Lukas <git@md5lukas.de>
2023-06-08 21:11:46 +0200

    Update to AnvilGUI 1.6.6

--- waypoints/src/main/kotlin/de/md5lukas/waypoints/util/AnvilGUIUtil.kt
+++ waypoints/src/main/kotlin/de/md5lukas/waypoints/util/AnvilGUIUtil.kt
@@ -1,5 +1,9 @@
 package de.md5lukas.waypoints.util
 
 import net.wesjd.anvilgui.AnvilGUI
+import org.bukkit.Material
 
-operator fun AnvilGUI.Completion.component1(): String = this.text
+operator fun AnvilGUI.StateSnapshot.component1(): Boolean =
+    outputItem === null || outputItem.type === Material.AIR
+
+operator fun AnvilGUI.StateSnapshot.component2(): String = text
