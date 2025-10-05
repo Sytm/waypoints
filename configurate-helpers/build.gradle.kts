0ae7cf08336e0f473d2a86882e8564bcfe58d7ba
Md5Lukas <git@md5lukas.de>
2025-10-05 09:48:57 +0200

    Publish configurate helpers

--- configurate-helpers/build.gradle.kts
+++ configurate-helpers/build.gradle.kts
@@ -1,9 +1,49 @@
-plugins { with(libs.plugins) { alias(kotlin) } }
+plugins {
+  with(libs.plugins) { alias(kotlin) }
+  `maven-publish`
+}
 
 kotlin { jvmToolchain(21) }
 
 dependencies {
-  implementation(libs.paper)
+  api(libs.paper)
   api(libs.configurate.core)
   api(libs.configurate.kotlin)
+}
+
+group = "de.md5lukas.configurate"
+
+version = project.property("configurateHelpersVersion") as String
+
+val sourcesJar by
+    tasks.registering(Jar::class) {
+      archiveClassifier = "sources"
+      from(sourceSets.main.get().allSource)
+    }
+
+publishing {
+  repositories {
+    maven {
+      name = "md5lukasReposilite"
+
+      url =
+          uri(
+              "https://repo.md5lukas.de/${
+            if (version.toString().endsWith("-SNAPSHOT")) {
+              "snapshots"
+            } else {
+              "releases"
+            }
+          }")
+
+      credentials(PasswordCredentials::class)
+      authentication { create<BasicAuthentication>("basic") }
+    }
+  }
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["kotlin"])
+      artifact(sourcesJar)
+    }
+  }
 }
